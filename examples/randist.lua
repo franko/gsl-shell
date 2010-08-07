7d88772e03a982b3e55982f39d7e8ddaf454654b
Francesco Abbate <francesco.bbt@gmail.com>
2010-08-08 01:00:37 +0200

    randist example plot added

--- examples/randist.lua
+++ examples/randist.lua
@@ -1,12 +1,17 @@
 
-pdf_test =
+demo1 =
    function()
-      local a, sig, n = 0.5, 2.44, 40
+      local a, sig, n = 2, 2.44, 40
       local idx = |k| a + (k-1)*5*sig/n -- sampling function
       local pdf = |x| pdf.gaussian_tail(x, a, sig)
-      local fs = {pdf, 
-		  |x| cdf.gaussian_tail(x, a, sig),
-		  |x| integ {f= pdf, points= {a, x}}
-	       }
-      return new(32, 3, |k,j| fs[j](idx(k)))
+      local cdf = |x| cdf.gaussian_tail(x, a, sig)
+      local cdftest = |x| integ {f= pdf, points= {a, x}}
+      local p = plot 'Gaussian tail pdf / cdf'
+      p:addline(fxline(pdf, a, 5*sig))
+      p:addline(fxline(cdf, a, 5*sig), 'blue')
+      p:addline(fxline(cdftest, a, 5*sig), 'green', {{'dash', 5, 5}})
+      p:show()
+      return p
    end
+
+print 'demo1() - gaussian tail distribution pdf and cdf plot'
