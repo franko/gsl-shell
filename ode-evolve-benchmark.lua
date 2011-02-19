c7ba92ec9a71454813b0c23eb6926f0342e3f757
francesco <francesco.bbt@gmail.com>
2011-02-19 18:28:50 +0100

    Added a benchmark to test GSL and LuaJIT2 implementations.

---
+++ ode-evolve-benchmark.lua
@@ -0,0 +1,24 @@
+
+local template = require 'template'
+
+local ode_spec = {N = 2, eps_abs = 1e-6, eps_rel = 0, a_y = 1, a_dydt = 0}
+
+local codegen = template.compile('rkf45.lua.in', ode_spec)
+local ode = codegen()
+
+function f_vanderpol_gen(mu)
+   return function(t, x, y) return y, -x + mu * y * (1-x^2) end
+end
+
+local f = f_vanderpol_gen(10.0)
+local s = ode.new()
+local x, y = 1, 0
+local t0, t1, h0 = 0, 20000, 0.01
+local init, evol = ode.init, ode.evolve
+for k=1, 10 do
+   init(s, t0, h0, f, x, y)
+   while s.t < t1 do
+      evol(s, f, t1)
+   end
+   print(s.t, s.y[0], s.y[1])
+end
--- ode-test.lua
+++ ode-test.lua
@@ -1,25 +1,10 @@
 
 local template = require 'template'
-local codegen = template.compile('rkf45.lua.in', {N = 2})
+
+local ode_spec = {N = 2, eps_abs = 1e-6, eps_rel = 0, a_y = 1, a_dydt = 0}
+
+local codegen = template.compile('rkf45.lua.in', ode_spec)
 local ode = codegen()
-
-local t0, t1, h0 = 0, 100, 1e-6
-
-function do_rk(y1, y2, f, sample)
-   local s = ode.new()
-
-   ode.init(s, t0, h0, f, y1, y2)
-
-   local tsamp = t0
-   while s.t < t1 do
-      ode.evolve(s, f, t1)
-      if sample and s.t - tsamp > sample.dt then
-	 sample.hook(s.t, s.y)
-	 tsamp = s.t
-      end
-   end
-   print(s.t, s.y)
-end
 
 function f_ode1(t, p, q)
    return -q - p^2,  2*p - q^3
@@ -29,37 +14,28 @@ function f_vanderpol_gen(mu)
    return function(t, x, y) return y, -x + mu * y * (1-x^2) end
 end
 
+local damp_f = 0.04
 function f_sincos_damp(t, s, c)
-   return c - 0.02*s, -s - 0.02*c
-end
-
-local function lnsample(lna, lnb)
-   return function(t, y)
-	     lna:line_to(t, y[0])
-	     lnb:line_to(t, y[1])
-	  end
-end
-
-local function xysample(ln)
-   return function(t, y)
-	     ln:line_to(y[0], y[1])
-	  end
+   return c - damp_f * s, -s - damp_f * c
 end
 
 function test1()
-   for k=1, 10 do
-      local f = f_sincos_damp
-      local x, y = 1, 0
-      lna = path(t0, x)
-      lnb = path(t0, y)
-      local sample = {dt = 0.1, hook= lnsample(lna, lnb)}
-      do_rk(x, y, f, sample)
-      local p = plot()
-      p:addline(lna)
-      p:addline(lnb, 'blue')
-      p:show()
-      break
+   local x, y = 1, 0
+   local s = ode.new()
+   local f = f_sincos_damp
+   local t0, t1, h0, hsamp = 0, 100, 1e-6, 0.5
+   ode.init(s, t0, h0, f, x, y)
+   local ln = path(t0, x)
+   for t= hsamp, t1, hsamp do
+      while s.t < t do
+	 ode.evolve(s, f, t)
+      end
+      ln:line_to(s.t, s.y[0])
    end
+   local p = plot()
+   p:addline(ln)
+   p:addline(fxline(|t| cos(t) * exp(- damp_f * t), 0, 100), 'blue', {{'dash', 7,3}})
+   p:show()
    return p
 end
 
@@ -82,9 +58,10 @@ function test2()
 end
 
 function test3()
-   local s, f = ode.new(), f_ode1
-   local x, y = 1, 1
-   local t0, t1, h0 = 0, 2000, 0.01
+   local f = f_vanderpol_gen(10.0)
+   local s = ode.new()
+   local x, y = 1, 0
+   local t0, t1, h0 = 0, 20000, 0.01
    for k=1, 10 do
       ode.init(s, t0, h0, f, x, y)
       while s.t < t1 do
@@ -97,7 +74,7 @@ end
 function test4()
    local f = f_vanderpol_gen(10.0)
    local x, y = 1, 0
-   local t0, t1, h0 = 0, 100, 1e-6
+   local t0, t1, h0 = 0, 5, 1e-6
 
    local s = ode.new()
    ode.init(s, t0, h0, f, x, y)
