d99ed66d9e16be1b3c1cca6b09d10a6ac7fb7add
Francesco Abbate <francesco.bbt@gmail.com>
2011-04-25 16:35:38 +0200

    Removed packages directory

--- packages/optical-disp/disp-library.h
+++
@@ -1,8 +0,0 @@
-#ifndef DISP_LIBRARY_H
-#define DISP_LIBRARY_H
-
-#include "lua.h"
-
-extern int disp_library_init (lua_State *L);
-
-#endif
--- packages/optical-disp/vector_float.h
+++
@@ -1,6 +0,0 @@
-
-#define BASE_TYPE float
-#include "vector_template_on.h"
-#include "vector_source.h"
-#include "vector_template_off.h"
-#undef BASE_TYPE
