5d6ade3d7faffa14aae1079d18490cc6015aca42
Francesco Abbate <francesco.bbt@gmail.com>
2011-05-02 00:24:31 +0200

    B-spline module update and interface modification

--- bspline.lua
+++ bspline.lua
@@ -4,6 +4,30 @@ local cgsl = require 'cgsl'
 
 local gsl_check = require 'gsl-check'
 
+local function eval(bs, x)
+   local dof, ws = bs.dof, bs.ws
+   local c = matrix.alloc(dof, 1)
+   local v = cgsl.gsl_matrix_column (c, 0)
+   gsl_check(cgsl.gsl_bspline_eval(x, v, ws))
+   return c
+end
+
+local function model(bs, x)
+   local dof, ws = bs.dof, bs.ws
+   local n = matrix.dim(x)
+   local m = matrix.alloc(n, dof)
+   for j = 0, n-1 do
+      local xj = cgsl.gsl_matrix_get(x, j, 0)
+      local v = cgsl.gsl_matrix_row (m, j)
+      gsl_check(cgsl.gsl_bspline_eval(xj, v, ws))
+   end
+   return m
+end
+
+local mt = {
+   __index = {eval= eval, model= model}
+}
+
 local function bspline(a, b, nbreak)
    local breaks
    local brk
@@ -23,25 +47,10 @@ local function bspline(a, b, nbreak)
       gsl_check(cgsl.gsl_bspline_knots_uniform (a, b, ws))
    end
 
-   local function eval(x)
-      local c = matrix.alloc(dof, 1)
-      local v = cgsl.gsl_matrix_column (c, 0)
-      gsl_check(cgsl.gsl_bspline_eval(x, v, ws))
-      return c
-   end
+   local bs = {dof= dof, ws= ws}
+   setmetatable(bs, mt)
 
-   local function model(x)
-      local n = matrix.dim(x)
-      local m = matrix.alloc(n, dof)
-      for j = 0, n-1 do
-	 local xj = x:get(j, 0)
-	 local v = cgsl.gsl_matrix_row (m, j)
-	 gsl_check(cgsl.gsl_bspline_eval(xj, v, ws))
-      end
-      return m
-   end
-
-   return {eval= eval, model= model}
+   return bs
 end
 
 return bspline
--- examples/bspline.lua
+++ examples/bspline.lua
@@ -7,14 +7,14 @@ function demo1()
    local n, br = 200, 10
 
    local f = |x| cos(x) * exp(-0.1 * x)
-   local xsmp = |i| 15 * i / (n-1)
+   local xsmp = |i| 15 * (i-1) / (n-1)
 
    local x = matrix.new(n, 1, xsmp)
    local y = matrix.new(n, 1, |i| f(xsmp(i)))
 
    local r = rng()
    local w = matrix.alloc(n, 1)
-   for i = 0, n-1 do 
+   for i = 1, n do 
       local yi = y[i]
       local sigma = 0.1 * yi
       y[i] = yi + rnd.gaussian(r, sigma)
@@ -22,7 +22,7 @@ function demo1()
    end
 
    local b = bspline(0, 15, br)
-   local X = b.model(x)
+   local X = b:model(x)
 
    local c, chisq, cov = linfit(X, y, w)
 
