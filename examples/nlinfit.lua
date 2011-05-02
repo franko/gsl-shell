b5891d324a52fa8be679cc716ac5b940cbce5163
francesco-ST <francesco.abbate@st.com>
2011-05-02 10:09:56 +0200

    Adapted example to lua style index convention

--- examples/nlinfit.lua
+++ examples/nlinfit.lua
@@ -4,24 +4,25 @@ use 'math'
 function demo1()
    local n = 40
 
-   local yrf, sigrf
+   local sigrf = 0.1
+   local yrf
 
    local fdf = function(x, f, J)
-		  for i=0, n-1 do
-		     local A, lambda, b = x[0], x[1], x[2]
-		     local t, y, sig = i, yrf[i], sigrf[i]
+		  for i=1, n do
+		     local A, lambda, b = x[1], x[2], x[3]
+		     local t, y, sig = i-1, yrf[i], sigrf
 		     local e = exp(- lambda * t)
 		     if f then f[i] = (A*e+b - y)/sig end
 		     if J then
-			J:set(i, 0, e / sig)
-			J:set(i, 1, - t * A * e / sig)
-			J:set(i, 2, 1 / sig)
+			J:set(i, 1, e / sig)
+			J:set(i, 2, - t * A * e / sig)
+			J:set(i, 3, 1 / sig)
 		     end
 		  end
 	       end
 
    local model = function(x, t)
-		    local A, lambda, b = x[0], x[1], x[2]
+		    local A, lambda, b = x[1], x[2], x[3]
 		    return A * exp(- lambda * t) + b
 		 end
 
@@ -30,8 +31,7 @@ function demo1()
    local r = gsl.rng('mt19937')
    r:set(0)
 
-   yrf = matrix.new(n, 1, function(i) return model(xref, i) + gsl.rnd.gaussian(r, 0.1) end)
-   sigrf = matrix.new(n, 1, function() return 0.1 end)
+   yrf = matrix.new(n, 1, |i| model(xref, i-1) + gsl.rnd.gaussian(r, 0.1))
 
    local s = gsl.nlinfit {n= n, p= 3}
 
@@ -45,7 +45,7 @@ function demo1()
    end
 
    local p = graph.plot('Non-linear fit example')
-   local pts = graph.ipath(gsl.sequence(function(i) return i, yrf[i] end, 0, n-1))
+   local pts = graph.ipath(gsl.sequence(function(i) return i-1, yrf[i] end, n))
    local fitln = graph.fxline(function(t) return model(s.x, t) end, 0, n-1)
    p:addline(pts, 'blue', {{'marker', size=5}})
    p:addline(fitln)
--- examples/vandermonde.lua
+++ examples/vandermonde.lua
@@ -1,7 +1,7 @@
 local function poly_eval(us, x)
    local s = 0
-   for i = 0, #us-1 do
-      s = s + us[i] * x^i
+   for i = 1, #us do
+      s = s + us[i] * x^(i-1)
    end
    return s
 end
@@ -9,13 +9,13 @@ end
 function demo1()
    local n = 7
    local r = gsl.rng()
-   local x = matrix.new(n, 1, |i| i+1)
+   local x = matrix.new(n, 1, |i| i)
    local y = matrix.new(n, 1, |i| r:get())
-   local m = matrix.new(n, n, |i,j| x[i]^j)
+   local m = matrix.new(n, n, |i,j| x[i]^(j-1))
    local us = matrix.solve(m, y)
    local p = graph.plot()
    p:addline(graph.xyline(x, y), 'blue', {{'marker', size=5}})
-   p:addline(graph.fxline(|x| poly_eval(us, x), x[0], x[#x-1]))
+   p:addline(graph.fxline(|x| poly_eval(us, x), x[1], x[#x]))
    p.title = 'Polynomial interpolation'
    p.clip = false
    p:show()
