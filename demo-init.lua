40c36698209f86dc1a9a51ab4d88da62ded69c10
Francesco Abbate <francesco.bbt@gmail.com>
2011-08-09 10:34:35 +0200

    Added demo to show animation capabilities

--- demo-init.lua
+++ demo-init.lua
@@ -16,7 +16,7 @@ local function load_demo(name)
 end
 
 local demo_files = {
-  'fft', 'bspline', 'wave-particle', 'plot', 'fractals', 'ode', 'nlinfit', 'integ'}
+  'fft', 'bspline', 'wave-particle', 'plot', 'fractals', 'ode', 'nlinfit', 'integ', 'anim'}
 
 for i, name in ipairs(demo_files) do
    load_demo(name)
