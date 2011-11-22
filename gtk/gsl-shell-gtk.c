83788136a3f4141dadbdf1eebd885ec0685409e3
Francesco Abbate <francesco.bbt@gmail.com>
2011-11-22 17:18:39 +0100

    Implement LuaJIT with a glib timeout function

--- gtk/gsl-shell-gtk.c
+++ gtk/gsl-shell-gtk.c
@@ -1,8 +1,8 @@
 #include <pthread.h>
 #include <unistd.h>
-#include <signal.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/ioctl.h>
 
 #include <gtk/gtk.h>
 #include <gdk/gdkkeysyms.h>
@@ -16,84 +16,31 @@ struct tinfo {
 
 static struct tinfo eval_info;
 
-static char output_buf[8192];
-static volatile int output_pos;
-static volatile int output_eof;
-
+static int stdout_save_fd;
 static int pipe_fd[2];
 
-pthread_mutex_t io_mutex = PTHREAD_MUTEX_INITIALIZER;
+static volatile gboolean luajit_running = FALSE;
+
+#define IO_TIMEOUT_MS 50
+
+#define BUF_SIZE 4096
+char output_buf[BUF_SIZE];
 
 lua_State *global_L;
 
 GtkTextBuffer *buffer;
 GtkTextMark *buffer_end_mark;
 
-static void
-sigio_handler (int sn)
-{
-  int nr;
-  char *buf;
-
-  pthread_mutex_lock (&io_mutex);
-
-  buf = output_buf + output_pos;
-
-  while (1)
-    {
-      errno = 0;
-      nr = read (pipe_fd[0], buf, 128);
-
-      if (nr == EOF)
-	{
-	  output_eof = 1;
-	  break;
-	}
-
-      if (nr <= 0)
-	break;
-
-      buf += nr;
-    }
-
-  output_pos = buf - output_buf;
-
-  pthread_mutex_unlock (&io_mutex);
-}
-
 static void *
 exec_line (void *_inf)
 {
   struct tinfo *inf = _inf;
-  int stdout_save_fd;
   fpos_t pos;
-  struct sigaction io_act;
-  struct sigaction io_act_old;
-  sigset_t block_mask;
-  pid_t current_pid;
 
-  output_eof = 0;
-  output_pos = 0;
-
-  sigemptyset (&block_mask);
-  io_act.sa_handler = sigio_handler;
-  io_act.sa_mask = block_mask;
-  io_act.sa_flags = SA_RESTART;
-  sigaction (SIGIO, &io_act, &io_act_old);
-  
   fflush (stdout);
   fgetpos (stdout, &pos);
-  stdout_save_fd = dup (fileno(stdout));
-
-  pipe (pipe_fd);
-
-  current_pid = getpid();
-
-  fcntl (pipe_fd[0], F_SETOWN, current_pid);
-  fcntl (pipe_fd[0], F_SETFL, O_ASYNC | O_NONBLOCK);
 
   dup2 (pipe_fd[1], fileno(stdout));
-  close (pipe_fd[1]);
 
   /* feed line to Lua interpreter */
   gsl_shell_exec (inf->L, inf->line);
@@ -101,15 +48,10 @@ exec_line (void *_inf)
   fflush (stdout);
 
   dup2 (stdout_save_fd, fileno(stdout));
-  close (stdout_save_fd);
   clearerr (stdout);
   fsetpos (stdout, &pos);
 
-  sleep(1);
-  close (pipe_fd[0]);
-
-  /* restore previous signal handler */
-  sigaction (SIGIO, &io_act_old, NULL);
+  luajit_running = FALSE;
 
   return NULL;
 }
@@ -136,52 +78,48 @@ start_eval_thread (lua_State *L, const char *line)
 }
 
 static gboolean
+retrieve_data(gpointer data)
+{
+  int nr;
+
+  fprintf (stderr, "inside retrieve data\n");
+  
+  do
+    {
+      nr = read (pipe_fd[0], output_buf, BUF_SIZE);
+      
+      if (nr > 0)
+	{
+	  GtkTextIter bpos[1];
+	  gtk_text_buffer_get_iter_at_mark (buffer, bpos, buffer_end_mark);
+	  gtk_text_buffer_insert (buffer, bpos, output_buf, nr);
+	}
+      
+    }
+  while (nr > 0);
+
+  if (!luajit_running)
+    {
+      int n_rem;
+      ioctl (pipe_fd[0], FIONREAD, &n_rem);
+      return (n_rem == 0 ? FALSE : TRUE);
+    }
+
+  return TRUE;
+}
+
+static gboolean
 on_key_pressed (GtkWidget *w, GdkEventKey *event, void *data)
 {
   if (event->keyval >= GDK_KEY_space || event->keyval <= GDK_KEY_asciitilde)
     {
       printf("%c\n", event->keyval);
+      luajit_running = TRUE;
+      g_timeout_add (IO_TIMEOUT_MS, retrieve_data, NULL);
       start_eval_thread (global_L, "for k=1, 12 do print(k^2) end");
     }
   
   return FALSE;
-}
-
-static gboolean
-retrieve_data(gpointer data)
-{
-  char *p;
-
-  pthread_mutex_lock (&io_mutex);
-
-  for (p = output_buf; p < output_buf + output_pos; )
-    {
-      char *pn = strchr (p, '\n');
-      if (!pn)
-	break;
-      p = pn + 1;
-    }
-
-  if (p > output_buf)
-    {
-      GtkTextIter bpos[1];
-      int len = p - output_buf;
-      int rem_len = output_pos - len;
-      int j;
-
-      gtk_text_buffer_get_iter_at_mark (buffer, bpos, buffer_end_mark);
-
-      gtk_text_buffer_insert (buffer, bpos, output_buf, len);
-  
-      for (j = 0; j < rem_len; j++)
-	output_buf[j] = output_buf[len+j];
-
-      output_pos = rem_len;
-    }
-
-  pthread_mutex_unlock (&io_mutex);
-
-  return TRUE;
 }
 
 int
@@ -190,6 +128,13 @@ main (int argc, char *argv[])
   GtkWidget *window;
   GtkWidget *view;
   GtkTextIter p[1];
+
+  stdout_save_fd = dup (fileno(stdout));
+
+  pipe (pipe_fd);
+
+  fcntl (pipe_fd[0], F_SETOWN, getpid());
+  fcntl (pipe_fd[0], F_SETFL, O_NONBLOCK);
 
   gtk_init (&argc, &argv);
 
@@ -215,20 +160,12 @@ main (int argc, char *argv[])
 
   gtk_container_add (GTK_CONTAINER (window), view);
 
-  g_timeout_add (100, retrieve_data, NULL);
-
   gtk_widget_show (view);
   gtk_widget_show (window);
 
   gtk_main ();
 
   gsl_shell_close (global_L);
-
-  output_buf[output_pos] = '\0';
-  printf("output buffer:\n");
-  puts(output_buf);
-
-  printf("> buffer end: %i\n", output_eof);
 
   return 0;
 }
