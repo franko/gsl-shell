27d0007092834f9b0b80030ac70d95b1e701a8b0
Francesco Abbate <francesco.bbt@gmail.com>
2011-11-23 15:05:55 +0100

    Add a scrolling bar for the LuaJIT console

--- gtk/gsl-shell-gtk.c
+++ gtk/gsl-shell-gtk.c
@@ -153,6 +153,7 @@ main (int argc, char *argv[])
 {
   GtkWidget *window;
   GtkWidget *view;
+  GtkWidget *swin;
   GtkTextBuffer *buffer;
   GtkTextIter p[1];
 
@@ -169,11 +170,15 @@ main (int argc, char *argv[])
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
-  gtk_window_set_default_size (window, 640, 480);
+  gtk_window_set_default_size (GTK_WINDOW (window), 640, 480);
 
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
+  swin = gtk_scrolled_window_new (NULL, NULL);
+  gtk_container_add (GTK_CONTAINER (window), swin);
   view = gtk_text_view_new ();
+  gtk_container_add (GTK_CONTAINER (swin), view);
+  gtk_scrolled_window_set_policy (swin, GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
 
   g_signal_connect (view, "key-press-event", G_CALLBACK (on_key_pressed), NULL);
 
@@ -188,8 +193,7 @@ main (int argc, char *argv[])
   repl.buffer = buffer;
   prepare_input ();
 
-  gtk_container_add (GTK_CONTAINER (window), view);
-
+  gtk_widget_show (swin);
   gtk_widget_show (view);
   gtk_widget_show (window);
 
