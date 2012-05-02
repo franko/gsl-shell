635c497e6382003eba7dbf71db0ad032c51e1172
Francesco Abbate <francesco.bbt@gmail.com>
2012-05-02 10:36:29 +0200

    Fix bug with unintentional copy of mutex

--- fox-gui/fox_gsl_shell.cpp
+++ fox-gui/fox_gsl_shell.cpp
@@ -29,7 +29,7 @@ void
 fox_gsl_shell::window_close_notify(int window_id)
 {
   fprintf(stderr, "got WINDOW CLOSE notification: %d\n", window_id);
-  pthread::mutex eval = eval_mutex();
+  pthread::mutex& eval = eval_mutex();
   eval.lock();
   m_window_close_queue.add(window_id);
   eval.unlock();
