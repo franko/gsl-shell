696ddad48760350ebba16562c749aca3f93659fd
Francesco Abbate <francesco.bbt@gmail.com>
2012-05-04 09:27:45 +0200

    Improve implementation of 'import' module
    Initially contributed by Lesley De Cruz.

--- import.lua
+++ import.lua
@@ -3,16 +3,11 @@ local _G, rawget, rawset = _G, rawget, rawset
 
 local modules_alias = {stdlib= {'math', 'matrix', 'iter', 'num', 'graph'}}
 
-local function new_env(self_hook) 
-   local lookup_modules = {}
-   local lookup_n = 0
+local function new_env()
+   local lookup_env = {}
+   local loader
 
    local function index(t, k)
-      for i= 1, lookup_n do
-	 local m = rawget(lookup_modules, i)
-	 local v = rawget(m, k)
-	 if v then return v end
-      end
       return rawget(_G, k)
    end
 
@@ -20,30 +15,32 @@ local function new_env(self_hook)
       rawset(_G, k, v)
    end
 
+   local mt = { __index= index, __newindex= newindex}
+
    local function load_module(module_name)
       local m = rawget(_G, module_name)
       if m and type(m) == 'table' then
-	 -- add the module in the lookup list
-	 table.insert(lookup_modules, m)
-	 lookup_n = # lookup_modules
+         for k,v in pairs(m) do
+	    rawset(lookup_env, k, v)
+         end
+	 if lookup_env.use ~= loader then lookup_env.use = loader end
       else
 	 error('module ' .. module_name .. ' not found')
       end
    end
 
-   local function loader(modname)
+   loader = function (modname)
       if modules_alias[modname] then
-	 for i, name in ipairs(modules_alias[modname]) do
-	    load_module(name)
-	 end
+   	 for i, name in ipairs(modules_alias[modname]) do
+   	    load_module(name)
+   	 end
       else
-	 load_module(modname)
+   	 load_module(modname)
       end
    end
 
-   local lookup_env = {[self_hook]= loader}
-
-   setmetatable(lookup_env, { __index= index, __newindex= newindex })
+   lookup_env.use = loader
+   setmetatable(lookup_env, mt)
 
    return lookup_env
 end
@@ -54,7 +51,7 @@ end
 
 function use(modname)
    local level = debug.getinfo(3, "") and 2 or 0
-   local env = new_env('use')
+   local env = new_env()
    env.use(modname)
    setfenv(level, env)
 end
