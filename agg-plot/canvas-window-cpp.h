25a38ca5c76151204a370ebac5b6b00aa5f9f42c
Francesco Abbate <francesco.bbt@gmail.com>
2012-06-23 15:27:07 +0200

    Don't hard code BGR24 pixel format in canvas_window class
    Refer instead to system wide gslshell::pixel_format.

--- agg-plot/canvas-window-cpp.h
+++ agg-plot/canvas-window-cpp.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <pthread.h>
 
+#include "agg-pixfmt-config.h"
 #include "platform_support_ext.h"
 #include "agg_trans_affine.h"
 #include "agg_color_rgba.h"
@@ -41,7 +42,7 @@ public:
   enum win_status_e status;
 
   canvas_window(agg::rgba& bgcol) :
-    platform_support_ext(agg::pix_format_bgr24, true),
+    platform_support_ext(gslshell::pixel_format, true),
     m_canvas(NULL), m_bgcolor(bgcol), m_matrix(), status(not_ready)
   { };
 
