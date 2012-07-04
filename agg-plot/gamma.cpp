afbff2213c6a6931b2fea818ef2c06b9873e4e63
Francesco Abbate <francesco.bbt@gmail.com>
2012-07-04 23:54:52 +0200

    Do not instrantiate gamma lut when not used

--- agg-plot/gamma.cpp
+++ agg-plot/gamma.cpp
@@ -1,4 +1,6 @@
 #include "agg-pixfmt-config.h"
 
+#ifndef DISABLE_GAMMA_CORR
 gslshell::gamma_type gslshell::gamma(1.5);
+#endif
 agg::lcd_distribution_lut gslshell::subpixel_lut(3./9., 2./9., 1./9.);
