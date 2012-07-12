eab8909093c6a5439f35167c9422c1a0934524f5
Francesco Abbate <francesco.bbt@gmail.com>
2012-07-12 09:54:21 +0200

    Change default weights for subpixel rendering

--- agg-plot/gamma.cpp
+++ agg-plot/gamma.cpp
@@ -3,4 +3,10 @@
 #ifndef DISABLE_GAMMA_CORR
 gslshell::gamma_type gslshell::gamma(1.5);
 #endif
-agg::lcd_distribution_lut gslshell::subpixel_lut(3./9., 2./9., 1./9.);
+// Original values for the subpixel color filter. These are quite conservative
+// to avoid color fringes.
+// agg::lcd_distribution_lut gslshell::subpixel_lut(3./9., 2./9., 1./9.);
+
+// Slightly more agressive values with more weight to primary channel.
+// Some people may notice colorer fringes but fonts looks sharper.
+agg::lcd_distribution_lut gslshell::subpixel_lut(0.448, 0.184, 0.092);
