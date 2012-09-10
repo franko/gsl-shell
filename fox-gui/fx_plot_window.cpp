7be7e40401aa1b77bc7fb1cf1020df2cfa146c39
Francesco Abbate <francesco.bbt@gmail.com>
2012-09-10 10:21:43 +0200

    Fix plot window's "close" menu command

--- fox-gui/fx_plot_window.cpp
+++ fox-gui/fx_plot_window.cpp
@@ -15,7 +15,7 @@ fx_plot_window::fx_plot_window(FXApp* app, const FXString& name, FXIcon *ic, FXI
     m_menu_bar = new FXMenuBar(this, LAYOUT_SIDE_TOP|LAYOUT_FILL_X);
 
     m_plot_menu = new FXMenuPane(this);
-    new FXMenuCommand(m_plot_menu, "&Close\tCtl-C", NULL, app, FXApp::ID_QUIT);
+    new FXMenuCommand(m_plot_menu, "&Close\tCtl-C", NULL, this, ID_CLOSE);
     new FXMenuTitle(m_menu_bar, "&Plot", NULL, m_plot_menu);
 
     FXVerticalFrame* frame = new FXVerticalFrame(this, LAYOUT_SIDE_TOP|LAYOUT_FILL_X|LAYOUT_FILL_Y);
