franko/gsl-shell

commit c171c08deebc353faa01e3869a4ec424ec264953

FOX: ensure cursor is always visible when browsing history

fox-gui/fx_console.cpp
@@ -145,6 +145,13 @@ long fx_console::update_editable()
     return 0;
 }
 
+void fx_console::update_input_line(const char* line)
+{
+    FXint line_len = get_input_length();
+    replaceText(m_input_begin, line_len, line, strlen(line));
+    makePositionVisible(getCursorPos());
+}
+
 long fx_console::on_key_press(FXObject* obj, FXSelector sel, void* ptr)
 {
     FXEvent* event = (FXEvent*)ptr;
@@ -200,8 +207,7 @@ long fx_console::on_key_press(FXObject* obj, FXSelector sel, void* ptr)
             extractText(m_saved_line, m_input_begin, line_len);
 
         const char* line = m_history.previous();
-        if (line)
-            replaceText(m_input_begin, line_len, line, strlen(line));
+        update_input_line(line);
 
         return 1;
     }
@@ -216,7 +222,7 @@ long fx_console::on_key_press(FXObject* obj, FXSelector sel, void* ptr)
         if (m_history.is_first())
             line = m_saved_line.text();
 
-        replaceText(m_input_begin, line_len, line, strlen(line));
+        update_input_line(line);
         return 1;
     }
     case KEY_Home:

fox-gui/fx_console.h
@@ -31,6 +31,7 @@ public:
 
     FXint get_input_length();
     long update_editable();
+    void update_input_line(const char* line);
 
     virtual void create();
 

commit f28a670a6a98436682474fdbd758f85677a3fb93

FOX: fix bug introduced with commit c171c08d

fox-gui/fx_console.cpp
@@ -207,7 +207,8 @@ long fx_console::on_key_press(FXObject* obj, FXSelector sel, void* ptr)
             extractText(m_saved_line, m_input_begin, line_len);
 
         const char* line = m_history.previous();
-        update_input_line(line);
+        if (line)
+            update_input_line(line);
 
         return 1;
     }
@@ -222,7 +223,8 @@ long fx_console::on_key_press(FXObject* obj, FXSelector sel, void* ptr)
         if (m_history.is_first())
             line = m_saved_line.text();
 
-        update_input_line(line);
+        if (line)
+            update_input_line(line);
         return 1;
     }
     case KEY_Home: