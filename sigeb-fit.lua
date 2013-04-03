55903fa657a614548604cf57fa699905440ea4d1
Francesco Abbate <francesco.bbt@gmail.com>
2013-04-03 16:42:06 +0200

    Implement newton iterative algorithm to solve XGE, B equations

--- sigeb-fit.lua
+++ sigeb-fit.lua
@@ -46,7 +46,7 @@ function param_plot(param, p, plot_pred)
 
 	local new_plot = (not p)
 	p = p or graph.plot()
-	p.title = param
+	p.title = param .. " vs XGE"
 	p:clear()
 	p:addline(graph.fxline(|x| fit:eval {XGE=x, B = 0}, -1, 1), 'red')
 	p:addline(graph.fxline(|x| fit:eval {XGE=x, B = -1}, -1, 1), 'blue')
@@ -55,6 +55,8 @@ function param_plot(param, p, plot_pred)
 	p:add(l_pred, "black", {{'marker', mark= "circle", size=6}})
 	if new_plot then
 		p.clip = false
+		p.xtitle = "XGE (normalized)"
+		p.ytitle = param
 		p:show()
 	end
 	return p, fit
@@ -69,17 +71,79 @@ param_plot "K_1(405.0nm)"
 ms:append("xge.ell.new")
 ms:append("b_conc.ell.new")
 
+local function f_k405(xge, b)
+	local t0 =   1.56531  + 0.0775967  * xge - 0.00244203 * xge^2
+	local t1 = -0.0198819 - 0.00369484 * xge - 0.0112398  * xge^2
+	local t2 = 0.00456136 + 0.00151712 * xge + 0.0123379  * xge^2
+	return t0 + t1*b + t2*b^2
+end
+
+local function f_n450(xge, b)
+	local t0 =  5.17391    + xge * 0.0462236 - xge^2 * 0.0309724
+	local t1 = -0.0788969  - xge * 0.0658174 - xge^2 * 0.0364469
+	local t2 = -0.00315506 - xge * 0.0211599 + xge^2 * 0.00411903
+	return t0 + t1*b + t2*b^2
+end
+
+
+local function f_k405_der(xge, b)
+	local t0 =   1.56531  + 0.0775967  * xge - 0.00244203 * xge^2
+	local t1 = -0.0198819 - 0.00369484 * xge - 0.0112398  * xge^2
+	local t2 = 0.00456136 + 0.00151712 * xge + 0.0123379  * xge^2
+	local d0 =  0.0775967  - 2 * 0.00244203 * xge
+	local d1 = -0.00369484 - 2 * 0.0112398  * xge
+	local d2 =  0.00151712 + 2 * 0.0123379  * xge
+	return d0 + d1*b + d2*b^2, t1 + 2*t2*b
+end
+
+local function f_n450_der(xge, b)
+	local t0 =  5.17391    + xge * 0.0462236 - xge^2 * 0.0309724
+	local t1 = -0.0788969  - xge * 0.0658174 - xge^2 * 0.0364469
+	local t2 = -0.00315506 - xge * 0.0211599 + xge^2 * 0.00411903
+	local d0 =  0.0462236 - 2 * xge * 0.0309724
+	local d1 = -0.0658174 - 2 * xge * 0.0364469
+	local d2 = -0.0211599 + 2 * xge * 0.00411903
+	return d0 + d1*b + d2*b^2, t1 + 2*t2*b
+end
+
+local function grid_search(k405, n450)
+	local score = 1000
+	local best = {}
+	for xge = -1, 1, 0.2 do
+		for b = -1, 1, 0.2 do
+			local f1 = f_k405(xge, b) - k405
+			local f2 = f_n450(xge, b) - n450
+			local fscore = f1^2 + f2^2
+			if fscore < score then
+				score = fscore
+				best.xge = xge
+				best.b   = b
+			end
+		end
+	end
+	return best.xge, best.b
+end
+
+local function newton_iter(k405, n450, xge, bc)
+	local a, b = f_k405_der(xge, bc)
+	local c, d = f_n450_der(xge, bc)
+	local f1 = - (f_k405(xge, bc) - k405)
+	local f2 = - (f_n450(xge, bc) - n450)
+	local del = a*d - b*c
+	return (f1 * d - f2 * b)/del + xge, (- f1 * c + f2 * a)/del + bc
+end
+
 for i = 1, #ms do
 	local k405 = ms:get(i, "K_1(405.0nm)")
 	local n450 = ms:get(i, "N_1(450.0nm)")
-	local xge = (k405 - 1.5753) / 0.106643
-	local a = -0.00315506 - xge * 0.0211599 + xge^2 * 0.00411903
-	local b = -0.0788969  - xge * 0.0658174 - xge^2 * 0.0364469
-	local c =  5.17391    + xge * 0.0462236 - xge^2 * 0.0309724 - n450
-	local del = max(b^2 - 4*a*c, 0)
-	local bconc = (-b - sqrt(del)) / (2*a)
+	local xge, b = grid_search(k405, n450)
+	print(i, 0, xge, b)
+	for k = 1, 3 do
+		xge, b = newton_iter(k405, n450, xge, b)
+		print(i, k, xge, b)
+	end
 	ms:set(i, "xge.ell.new", xge * 5 + 35)
-	ms:set(i, "b_conc.ell.new", bconc * 6e20 + 8e20)
+	ms:set(i, "b_conc.ell.new", b * 6e20 + 8e20)
 end
 
 gdt.plot(ms, "xge.sims, xge.ell.new ~ wafer, site")
