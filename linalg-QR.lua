ac7c369abc7d72af757dabaff445ec3a6bcc09b6
Francesco Abbate <francesco.bbt@gmail.com>
2013-05-22 19:18:37 +0200

    Implement function to perform QR decomposition

--- linalg-QR.lua
+++ linalg-QR.lua
@@ -2,6 +2,30 @@ local ffi = require 'ffi'
 local gsl = require 'gsl'
 local gsl_check = require 'gsl-check'
 
+local function QR(A)
+	local m, n = A:dim()
+	local Ac = A:copy()
+	local Q = matrix.unit(m)
+	local k = math.min(m, n)
+	local tau = gsl.gsl_vector_alloc(k)
+
+	gsl.gsl_linalg_QR_decomp(Ac, tau)
+
+	for j = 0, m - 1 do
+		local v = gsl.gsl_matrix_column(Q, j)
+		gsl.gsl_linalg_QR_Qvec(Ac, tau, v)
+	end
+
+	for j = 0, n - 1 do
+		for i = j + 1, m - 1 do
+			Ac.data[i*n + j] = 0
+		end
+	end
+
+	gsl.gsl_vector_free(tau)
+	return Q, Ac
+end
+
 local function QRPT(A)
 	local m, n = A:dim()
 	local q = matrix.alloc(m, m)
@@ -17,4 +41,4 @@ local function QRPT(A)
 	return q, r, tau, p
 end
 
-return QRPT
+return {QR= QR, QRPT= QRPT}
