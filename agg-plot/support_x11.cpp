f4b691e9f9c0eaabf4057451e11ad1a5a91151d5
Francesco Abbate <francesco.bbt@gmail.com>
2013-05-29 15:54:26 +0200

    Fix truetype font search to work across linux distributions

--- agg-plot/support_x11.cpp
+++ agg-plot/support_x11.cpp
@@ -4,28 +4,40 @@
 
 #ifdef DARWIN_MACOSX
 const char *ttf_names[] = {"Arial.ttf", "Trebuchet MS.ttf", "Courier New.ttf", 0};
-#define TTF_SYSTEM_DIR "/Library/Fonts/"
+const char *ttf_system_dir[] = {"/Library/Fonts", 0};
 #define CONSOLE_FONT_NAME "Monaco"
 #else
-const char *ttf_names[] = {"ubuntu-font-family/Ubuntu-R.ttf", "freefont/FreeSans.ttf", "ttf-dejavu/DejaVuSans.ttf", 0};
-#define TTF_SYSTEM_DIR "/usr/share/fonts/truetype/"
+const char *ttf_names[] = {
+    "ubuntu-font-family/Ubuntu-R.ttf",
+    "freefont/FreeSans.ttf", "freefont-ttf/FreeSans.ttf", "FreeSans.ttf",
+    "ttf-dejavu/DejaVuSans.ttf", "dejavu/DejaVuSans.ttf", "DejaVuSans.ttf",
+    "liberation/LiberationSans-Regular.ttf", 0};
+const char *ttf_system_dir[] = {"/usr/share/fonts/truetype", "/usr/share/fonts", "/usr/share/fonts/TTF", 0};
 #define CONSOLE_FONT_NAME "monospace"
 #endif
 
 const char *gslshell::get_font_name()
 {
-    const char* ttf_dir = TTF_SYSTEM_DIR;
-    unsigned len = strlen(ttf_dir);
-    static char pf[256];
-    memcpy(pf, ttf_dir, len + 1);
-    for (int k = 0; ttf_names[k]; k++)
+    for (int i = 0; ttf_system_dir[i]; i++)
     {
-        const char* font_name = ttf_names[k];
-        memcpy(pf + len, font_name, (strlen(font_name) + 1) * sizeof(char));
-        struct stat inf[1];
-        int status = stat(pf, inf);
-        if (status == 0)
-            return pf;
+        const char* ttf_dir = ttf_system_dir[i];
+        unsigned len = strlen(ttf_dir);
+        static char pf[256];
+        memcpy(pf, ttf_dir, len);
+        char* base_ptr = pf + len;
+        if (pf[len - 1] != '/') {
+            *base_ptr = '/';
+            base_ptr ++;
+        }
+        for (int k = 0; ttf_names[k]; k++)
+        {
+            const char* font_name = ttf_names[k];
+            memcpy(base_ptr, font_name, (strlen(font_name) + 1) * sizeof(char));
+            struct stat inf[1];
+            int status = stat(pf, inf);
+            if (status == 0)
+                return pf;
+        }
     }
 
     return 0;
