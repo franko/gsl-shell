f56785a3cb8d8e44ecb860e50d9ddd2f59dcc036
Francesco Abbate <francesco.bbt@gmail.com>
2013-06-27 00:36:04 +0200

    Improve implementation of column matrix quicksort

--- matrix-quicksort.lua
+++ matrix-quicksort.lua
@@ -1,26 +1,90 @@
 
-local function quicksort_array(m, f)
-	local t = m.data
+local floor = math.floor
 
-    local function quicksort(start, endi)
-        if start >= endi then return end
-        local pivot = start
-        for i = start + 1, endi do
-            if f(t[i], t[pivot]) then
-                local temp = t[pivot + 1]
-                t[pivot + 1] = t[pivot]
-                if(i == pivot + 1) then
-                    t[pivot] = temp
-                else
-                    t[pivot] = t[i]
-                    t[i] = temp
-                end
-                pivot = pivot + 1
+local insertion_thresold = 10
+
+local function less_than(a, b)
+    return a < b
+end
+
+local function idiv(a, b)
+    return floor(a / b)
+end
+
+local function insertion_sort(array, compare, istart, iend)
+    for i = istart + 1, iend do
+        local current_value = array[i]
+        local hole_index = i
+        while hole_index > istart and compare(current_value, array[hole_index - 1]) do
+            array[hole_index] = array[hole_index - 1]
+            hole_index = hole_index - 1
+        end
+        array[hole_index] = current_value
+    end
+end
+
+local function quicksort_array(m, f)
+    local array = m.data
+
+    f = f or less_than
+
+    local function less_than_by_index(ia, ib)
+        return f(array[ia], array[ib])
+    end
+
+    local function partition(istart, iend, pivot_index)
+        local pivot_value = array[pivot_index]
+        array[pivot_index], array[iend] = array[iend], array[pivot_index]
+        local store_index = istart
+        for i = istart, iend - 1 do
+            if f(array[i], pivot_value) then
+                array[i], array[store_index] = array[store_index], array[i]
+                store_index = store_index + 1
             end
         end
+        array[store_index], array[iend] = array[iend], array[store_index]
+        return store_index
+    end
 
-        quicksort(start, pivot - 1)
-        quicksort(pivot + 1, endi)
+    local function choose_pivot(istart, iend)
+        local n = iend - istart + 1
+        if n < 5 then
+            return istart
+        elseif n < 64 then
+            print("using three values median")
+            local imid = idiv(istart + iend, 2)
+            local indices = {istart, imid, iend}
+            insertion_sort(indices, less_than_by_index, 1, 3)
+            print(indices, iter.ilist(|i| array[indices[i]], 3))
+            return indices[2]
+        else
+            print("using five values median")
+            local im1 = idiv(3*istart +   iend, 4)
+            local im2 = idiv(2*istart + 2*iend, 4)
+            local im3 = idiv(  istart + 3*iend, 4)
+            local indices = {istart, im1, im2, im3, iend}
+            insertion_sort(indices, less_than_by_index, 1, 5)
+            print(indices, iter.ilist(|i| array[indices[i]], 5))
+            return indices[3]
+        end
+    end
+
+    local function quicksort(istart, iend)
+        local n = iend - istart + 1
+        if n < insertion_thresold then
+            print("fallback to insertion sort, n:", n)
+            insertion_sort(array, f, istart, iend)
+        else
+            local pivot_index = choose_pivot(istart, iend)
+            local part_index = partition(istart, iend, pivot_index)
+            print("quicksort with partitions:", part_index - istart, iend - part_index)
+            if part_index - istart == 0 then -- odd case, mostly happens for constant array
+                insertion_sort(array, f, istart, iend)
+            else
+                quicksort(istart, part_index - 1)
+                quicksort(part_index + 1, iend)
+            end
+        end
     end
 
     local n, ncols = m:dim()
