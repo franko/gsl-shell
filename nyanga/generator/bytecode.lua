franko/gsl-shell

commit 3b9fc957941ae8da40d701ed546a798bc52207e4

Implement specialized instructions for equality tests

nyanga/generator/bytecode.lua
@@ -836,7 +836,18 @@ local function generate(tree, name)
    end
 
    function self:expr_emit_testop(node)
-      -- TODO: this is just a sub implementation
+      if is_literal(node) then
+         local value = node.value
+         local tp = type(value)
+         if tp == 'string' then
+            return 'S', self.ctx:const(value)
+         elseif tp == 'number' then
+            return 'N', self.ctx:const(value)
+         elseif tp == 'boolean' or tp == nil then
+            return 'P', self.ctx:kpri(value)
+         end
+         -- fall through
+      end
       return 'V', self:expr_emit(node)
    end
 

commit 8aeed2f4fc3d68897cba25411971b8a678a9ca81

Fix bug introduced with commit 3b9fc957

nyanga/generator/bytecode.lua
@@ -312,7 +312,7 @@ function TestRule:BinaryExpression(node, jmp, negate, store, dest)
    if cmpop[o] then
       local free = self.ctx.freereg
       local a = self:expr_emit(node.left)
-      local btag, b = self:expr_emit_testop(node.right)
+      local btag, b = self:expr_emit_testop(o, node.right)
       self.ctx.freereg = free
       local use_imbranch = (dest and has_branch(store, negate))
       if use_imbranch then
@@ -803,8 +803,8 @@ local function generate(tree, name)
       end
    end
 
-   function self:expr_emit_testop(node)
-      if is_literal(node) then
+   function self:expr_emit_testop(o, node)
+      if (o == 'NE' or o == 'EQ') and is_literal(node) then
          local value = node.value
          local tp = type(value)
          if tp == 'string' then