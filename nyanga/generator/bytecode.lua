franko/gsl-shell

commit a3c4a48da7cb513a8b422003e34acb443619955b

Fix statements' emit rules for If expressions

nyanga/generator/bytecode.lua
@@ -483,9 +483,9 @@ function StatementRule:GotoStatement(node)
    return self.ctx:jump(node.label)
 end
 
-function StatementRule:BlockStatement(node)
+function StatementRule:BlockStatement(node, ...)
    for i=1, #node.body do
-      self:emit(node.body[i])
+      self:emit(node.body[i], ...)
    end
 end
 
@@ -495,26 +495,26 @@ function StatementRule:DoStatement(node)
    self:block_leave()
 end
 
-function StatementRule:IfStatement(node, nest, exit)
+function StatementRule:IfStatement(node, root_exit)
    local free = self.ctx.freereg
-   exit = exit or util.genid()
+   local local_exit = node.alternate and util.genid()
+   local exit = root_exit or local_exit
    local altl = util.genid()
    if node.test then
       self:test_emit(node.test, altl)
    end
 
-   local block_exit = node.alternate and exit
    self:block_enter()
-   self:emit(node.consequent)
-   self:block_leave(block_exit)
+   self:emit(node.consequent, exit)
+   self:block_leave(exit)
 
    self.ctx:here(altl)
    if node.alternate then
       self:block_enter()
-      self:emit(node.alternate, true, exit)
+      self:emit(node.alternate)
       self:block_leave()
    end
-   if not nest then
+   if exit and exit == local_exit then
       self.ctx:here(exit)
    end
    self.ctx.freereg = free

commit 637f3276dbbfd0ad2dd7caff809892b463d144b6

Fix bug introduced with commit a3c4a48d

nyanga/generator/bytecode.lua
@@ -491,7 +491,12 @@ end
 
 function StatementRule:BlockStatement(node, ...)
    for i=1, #node.body do
-      self:emit(node.body[i], ...)
+      local s = node.body[i]
+      if i < #node.body then
+         self:emit(s)
+      else
+         self:emit(s, ...)
+      end
    end
 end
 