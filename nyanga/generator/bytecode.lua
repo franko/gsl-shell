franko/gsl-shell

commit b6a3dc50fea9bd5e71b369aa0d1f93227fc56782

Implement LuaJIT bytecode gen compatibility fix

nyanga/generator/bytecode.lua
@@ -399,6 +399,9 @@ function TestRule:BinaryExpression(node, jmp, negate, store, dest)
          self.ctx:op_load(dest, not negate)
       end
    else
+      -- LuaJIT compatibility rule: set store to a non zero
+      -- value so that the register is counted on test jump
+      store, dest = EXPR_RESULT_BOTH, self.ctx.freereg
       self:expr_test(node, jmp, negate, store, dest)
    end
 end
@@ -856,6 +859,9 @@ local function generate(tree, name)
       if rule then
          rule(self, node, jmp, negate, store, dest)
       else
+         -- LuaJIT compatibility rule: set store to a non zero
+         -- value so that the register is counted on test jump
+         store, dest = EXPR_RESULT_BOTH, self.ctx.freereg
          self:expr_test(node, jmp, negate, store, dest)
       end
    end

commit d34eba9bbf53f17b8cb0ca3f655f62320f3ebfc2

Fix bug introduced with commit b6a3dc50

nyanga/generator/bytecode.lua
@@ -401,7 +401,7 @@ function TestRule:BinaryExpression(node, jmp, negate, store, dest)
    else
       -- LuaJIT compatibility rule: set store to a non zero
       -- value so that the register is counted on test jump
-      store, dest = EXPR_RESULT_BOTH, self.ctx.freereg
+      store, dest = EXPR_RESULT_BOTH, dest or self.ctx.freereg
       self:expr_test(node, jmp, negate, store, dest)
    end
 end
@@ -861,7 +861,7 @@ local function generate(tree, name)
       else
          -- LuaJIT compatibility rule: set store to a non zero
          -- value so that the register is counted on test jump
-         store, dest = EXPR_RESULT_BOTH, self.ctx.freereg
+         store, dest = EXPR_RESULT_BOTH, dest or self.ctx.freereg
          self:expr_test(node, jmp, negate, store, dest)
       end
    end