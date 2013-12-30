6c759a1a9080050503f3a1bcc18adfd76b0f9a38
Francesco Abbate <francesco.bbt@gmail.com>
2013-12-30 14:12:28 +0100

    Fix problems with the lexer

--- nyanga/lex-test.lua
+++ nyanga/lex-test.lua
@@ -4,9 +4,29 @@ local ASCII_0, ASCII_9 = 48, 57
 local ASCII_a, ASCII_z = 97, 122
 local ASCII_A, ASCII_Z = 65, 90
 
+local tricky_string = 'ciao \x6aason \106ack \t\r\n\v'
+
 local END_OF_STREAM = -1
 
 local ReservedKeyword = {['and'] = 1, ['break'] = 2, ['do'] = 3, ['else'] = 4, ['elseif'] = 5, ['end'] = 6, ['false'] = 7, ['for'] = 8, ['function'] = 9, ['goto'] = 10, ['if'] = 11, ['in'] = 12, ['local'] = 13, ['nil'] = 14, ['not'] = 15, ['or'] = 16, ['repeat'] = 17, ['return'] = 18, ['then'] = 19, ['true'] = 20, ['until'] = 21, ['while'] = 22 }
+
+local function foo(...)
+    return select('#', ...)
+end
+
+local some_x = .5
+
+local raw_string = [[C:\temp\somefile.dat]]
+local long_string = [[
+   Some very long
+   string
+   here! "I mean"
+]]
+
+--[[
+   A very long comment
+   HERE!
+]]
 
 setmetatable(_G, {
     __index = function(t, x) error('undefined global ' .. x) end,
