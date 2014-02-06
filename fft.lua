478e835df7826064944a072730c87d4d1c614e33
Benjamin von Ardenne <Benjamin.von.Ardenne@gmail.com>
2014-02-06 22:41:41 +0100

    Added plans for optimized FFTs

--- fft.lua
+++ fft.lua
@@ -1,49 +1,48 @@
-fftw = require'fftw3.init'
 ffi = require'ffi'
+fftw = require'fftw3.init'
 local gsl_matrix = ffi.typeof('gsl_matrix')
 local gsl_matrix_complex = ffi.typeof('gsl_matrix_complex')
 
 function num.fft(vec)
-	if ffi.istype(gsl_matrix, vec) then
-		return num.rfft(vec)
-	else
+	if ffi.istype(gsl_matrix_complex, vec) then
 		local n = tonumber(vec.size1)
-		local outvec = matrix.cnew(n, 1)
+		local outvec = matrix.calloc(n, 1)
 		local output = ffi.cast("fftw_complex*",outvec.data)
 		local input = ffi.cast("fftw_complex*",vec.data)
-		local plan = fftw.plan_dft_1d(n, input, output, fftw.FORWARD, fftw.MEASURE)
+
+		local plan = ffi.gc(fftw.plan_dft_1d(n, input, output, fftw.FORWARD, fftw.MEASURE), fftw.destroy_plan)
 		fftw.execute(plan)
-		--fftw.destroy_plan(plan)
-		return outvec
+		return outvec, plan
+	else
+		error("Input must be complex valued.")
 	end
 end
 
 function num.fftinv(vec)
-	if ffi.istype(gsl_matrix, vec) then
-		return num.rfft_inv(vec)
-	else
+	if ffi.istype(gsl_matrix_complex, vec) then
 		local n = tonumber(vec.size1)
-		local outvec = matrix.cnew(n, 1)
+		local outvec = matrix.calloc(n, 1)
 		local output = ffi.cast("fftw_complex*",outvec.data)
 		local input = ffi.cast("fftw_complex*",vec.data)
-		local plan = fftw.plan_dft_1d(n, input, output, fftw.BACKWARD, fftw.MEASURE)
+
+		local plan = ffi.gc(fftw.plan_dft_1d(n, input, output, fftw.BACKWARD, fftw.MEASURE), fftw.destroy_plan)
 		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outvec/n
+		return outvec, plan
+	else
+		error("Input must be complex valued.")
 	end
 end
 
 function num.rfft(vec)
 	if ffi.istype(gsl_matrix, vec) then
 		local n = tonumber(vec.size1)
-		local outvec = matrix.cnew(math.floor(n/2)+1, 1)
+		local outvec = matrix.calloc(math.floor(n/2)+1, 1)
 		local output = ffi.cast("fftw_complex*",outvec.data)
 		local plan = fftw.plan_dft_r2c_1d(n, vec.data, output, fftw.MEASURE )
 		fftw.execute(plan)
-		--fftw.destroy_plan(plan)
-		return outvec
+		return outvec, plan
 	else
-		error("Cannot handle nonreal input data.")
+		error("Input must be real valued.")
 	end
 end
 
@@ -51,48 +50,45 @@ function num.rfftinv(vec)
 	if ffi.istype(gsl_matrix_complex, vec) then
 		local n = tonumber(vec.size1)
 		local nnew = (n-1)*2
-		local outvec = matrix.new(nnew, 1)
+		local outvec = matrix.alloc(nnew, 1)
 		local input = ffi.cast("fftw_complex*",vec.data)
 		local plan = fftw.plan_dft_c2r_1d(nnew, input, outvec.data, fftw.MEASURE )
 		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outvec/nnew
+		return outvec,plan
 	else
-		error("Cannot handle noncomplex input data.")
+		error("Input must be complex valued.")
 	end
 end
 
 ----------------------------------
 
 function num.fft2(mat)
-	if ffi.istype(gsl_matrix, mat) then
-		return num.rfft2(mat)
-	else
+	if ffi.istype(gsl_matrix_complex, mat) then
 		local n1 = tonumber(mat.size1)
 		local n2 = tonumber(mat.size2)
-		local outmat = matrix.cnew(n1, n2)
+		local outmat = matrix.calloc(n1, n2)
 		local output = ffi.cast("fftw_complex*",outmat.data)
 		local input = ffi.cast("fftw_complex*", mat.data)
 		local plan = fftw.plan_dft_2d(n1,n2, input, output, fftw.FORWARD, fftw.MEASURE)
 		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outmat
+		return outmat, plan
+	else
+		error("Input must be complex valued.")
 	end
 end
 
 function num.fft2inv(mat)
-	if ffi.istype(gsl_matrix, mat) then
-		return num.rfft2_inv(mat)
-	else
+	if ffi.istype(gsl_matrix_complex, mat) then
 		local n1 = tonumber(mat.size1)
 		local n2 = tonumber(mat.size2)
-		local outmat = matrix.cnew(n1, n2)
+		local outmat = matrix.calloc(n1, n2)
 		local output = ffi.cast("fftw_complex*",outmat.data)
 		local input = ffi.cast("fftw_complex*", mat.data)
 		local plan = fftw.plan_dft_2d(n1,n2, input, output, fftw.BACKWARD, fftw.MEASURE)
 		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outmat
+		return outmat, plan
+	else
+		error("Input must be complex valued.")
 	end
 end
 
@@ -100,14 +96,13 @@ function num.rfft2(mat)
 	if ffi.istype(gsl_matrix, mat) then
 		local n1 = tonumber(mat.size1)
 		local n2 = tonumber(mat.size2)
-		local outmat = matrix.cnew(n1, math.floor(n2/2)+1)
+		local outmat = matrix.calloc(n1, math.floor(n2/2)+1)
 		local output = ffi.cast("fftw_complex*",outmat.data)
 		local plan = fftw.plan_dft_r2c_2d(n1,n2, mat.data, output,fftw.MEASURE)
 		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outmat
+		return outmat, plan
 	else
-		error("Cannot handle nonreal input matrix.")
+		error("Input must be real valued.")
 	end
 end
 
@@ -116,138 +111,95 @@ function num.rfft2inv(mat)
 		local n1 = tonumber(mat.size1)
 		local n2 = tonumber(mat.size2)
 		local n2new = (n2-1)*2
-		local outmat = matrix.new(n1, n2new)
+		local outmat = matrix.alloc(n1, n2new)
 		local input = ffi.cast("fftw_complex*",mat.data)
 		local plan = fftw.plan_dft_c2r_2d(n1,n2new, input, outmat.data,fftw.MEASURE)
 		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outmat/(n1*n2new)
+		return outmat, plan
 	else
-		error("Cannot handle noncomplex input matrix.")
+		error("Input must be complex valued.")
 	end
 end
 
 --------------------------------------
 
-function num.fft3(datavec, n1, n2, n3)
-	if ffi.istype(gsl_matrix, datavec) then
-		return num.rfft3(datavec, n1, n2, n3)
-	else
-		local outvec = matrix.cnew(n1*n2*n3, 1)
+function num.fftn(datavec, dimvec)
+	if ffi.istype(gsl_matrix_complex, datavec) then
+		local outvec = matrix.calloc(tonumber(datavec.size1), 1)
 		local output = ffi.cast("fftw_complex*",outvec.data)
 		local input = ffi.cast("fftw_complex*", datavec.data)
-		local plan = fftw.plan_dft_3d(n1,n2,n3, input, output, fftw.FORWARD, fftw.MEASURE)
+		local plan = fftw.plan_dft(#dimvec, dimvec.data, input, output, fftw.FORWARD, fftw.MEASURE)
 		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outvec
+		return outvec, plan
+	else
+		error("Input must be complex valued.")
 	end
 end
 
-function num.fft3inv(datavec, n1, n2, n3)
-	if ffi.istype(gsl_matrix, datavec) then
-		return num.rfft3_inv(datavec, n1, n2, n3)
-	else
-		local outvec = matrix.cnew(n1*n2*n3, 1)
+function num.fftninv(datavec, dimvec)
+	if ffi.istype(gsl_matrix_complex, datavec) then
+		local outvec = matrix.calloc(tonumber(datavec.size1), 1)
 		local output = ffi.cast("fftw_complex*",outvec.data)
 		local input = ffi.cast("fftw_complex*", datavec.data)
-		local plan = fftw.plan_dft_3d(n1,n2,n3, input, output, fftw.BACKWARD, fftw.MEASURE)
+		local plan = fftw.plan_dft(#dimvec, dimvec.data, input, output, fftw.BACKWARD, fftw.MEASURE)
 		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outvec/(n1*n2*n3)
-	end
-end
-
-function num.rfft3(datavec, n1, n2, n3)
-	if ffi.istype(gsl_matrix, datavec) then
-		local outvec = matrix.cnew(n1*n2*(math.floor(n3/2)+1), 1)
-		local output = ffi.cast("fftw_complex*",outvec.data)
-		local plan = fftw.plan_dft_r2c_3d(n1,n2,n3, datavec.data, output, fftw.MEASURE)
-		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outvec
+		return outvec, plan
 	else
-		error("Cannot handle nonreal input data.")
+		error("Input must be complex valued.")
 	end
 end
 
-function num.rfft3inv(datavec, n1, n2, n3)
+function num.rfftn(datavec, dimvec)
 	if ffi.istype(gsl_matrix, datavec) then
-		local outvec = matrix.new(n1*n2*n3, 1)
-		local input = ffi.cast("fftw_complex*",datavec.data)
-		local plan = fftw.plan_dft_r2c_3d(n1,n2,n3, input, outvec.data, fftw.MEASURE)
-		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outvec/(n1*n2*n3)
-	else
-		error("Cannot handle nonreal input data.")
-	end
-end
-
---------------------------------------
-
-function num.fftn(datavec, dimensionvec)
-	if ffi.istype(gsl_matrix, datavec) then
-		return num.rfftn(datavec, dimensionvec)
-	else
-		local outvec = matrix.cnew(tonumber(datavec.size1), 1)
-		local output = ffi.cast("fftw_complex*",outvec.data)
-		local input = ffi.cast("fftw_complex*", datavec.data)
-		local plan = fftw.plan_dft(#dimensionvec, dimensionvec.data, input, output, fftw.FORWARD, fftw.MEASURE)
-		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outvec
-	end
-end
-
-function num.fftninv(datavec, dimensionvec)
-	if ffi.istype(gsl_matrix, datavec) then
-		return num.rfftn_inv(datavec, dimensionvec)
-	else
-		local outvec = matrix.cnew(tonumber(datavec.size1), 1)
-		local output = ffi.cast("fftw_complex*",outvec.data)
-		local input = ffi.cast("fftw_complex*", datavec.data)
-		local plan = fftw.plan_dft(#dimensionvec, dimensionvec.data, input, output, fftw.BACKWARD, fftw.MEASURE)
-		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outvec
-	end
-end
-
-function num.rfftn(datavec, dimensionvec)
-	if ffi.istype(gsl_matrix, datavec) then
-		local d = #dimensionvec
+		local d = #dimvec
 		local newsize = 1
 		for i=1,d-1 do
-			newsize = newsize * dimensionvec[i]
+			newsize = newsize * dimvec[i]
 		end
-		newsize = newsize * math.floor(dimensionvec[d]/2)+1
+		newsize = newsize * math.floor(dimvec[d]/2)+1
 
-		local outvec = matrix.cnew(newsize, 1)
+		local outvec = matrix.calloc(newsize, 1)
 		local output = ffi.cast("fftw_complex*",outvec.data)
-		local plan = fftw.plan_dft_r2c(d, dimensionvec.data, datavec.data, output, fftw.MEASURE)
+		local plan = fftw.plan_dft_r2c(d, dimvec.data, datavec.data, output, fftw.MEASURE)
 		fftw.execute(plan)
-		fftw.destroy_plan(plan)
-		return outvec
+		return outvec, plan
 	else
-		error("Cannot handle nonreal input data.")
+		error("Input must be real valued.")
 	end
 end
 
-function num.rfftninv(datavec, dimensionvec)
-	if ffi.istype(gsl_matrix, datavec) then
-		local d = #dimensionvec
+function num.rfftninv(datavec, dimvec)
+	if ffi.istype(gsl_matrix_complex, datavec) then
+		local d = #dimvec
 		local newsize = 1
 		for i=1,d do
-			newsize = newsize * dimensionvec[i]
+			newsize = newsize * dimvec[i]
 		end
 
-		local outvec = matrix.new(newsize, 1)
+		local outvec = matrix.alloc(newsize, 1)
 		local output = ffi.cast("fftw_complex*",outvec.data)
-		local plan = fftw.plan_dft_r2c(d, dimensionvec.data, datavec.data, output, fftw.MEASURE)
+		local plan = fftw.plan_dft_r2c(d, dimvec.data, datavec.data, output, fftw.MEASURE)
 		fftw.execute(plan)
-		fftw.destroy_plan(plan)
 		return outvec
 	else
-		error("Cannot handle nonreal input data.")
+		error("Input must be complex valued.")
 	end
+end
+
+------------------------------------------
+
+function num.fft_plan(plan, input, output)
+	local fftw_in = ffi.cast("fftw_complex*",input.data)
+	local fftw_out = ffi.cast("fftw_complex*",output.data)
+	return fftw.execute_dft(plan, fftw_in, fftw_out)
+end
+
+function num.rfft_plan(plan, input, output)
+	local fftw_out = ffi.cast("fftw_complex*",output.data)
+	return fftw.execute_dft_r2c(plan, input.data, fftw_out)
+end
+
+function num.rfftinv_plan(plan, input, output)
+	local fftw_in = ffi.cast("fftw_complex*",input.data)
+	return fftw.execute_dft_c2r(plan, fftw_in, output.data)
 end
