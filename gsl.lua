d15d0acf733d0642bf6f78dff392a9ff608f5007
Francesco <francesco.bbt@gmail.com>
2016-10-02 23:03:51 +0200

    Add GSL wavelets C declarations from GSL 2

--- gsl.lua
+++ gsl.lua
@@ -2657,8 +2657,67 @@ size_t
 gsl_interp_bsearch(const double x_array[], double x,
                    size_t index_lo, size_t index_hi);
 
-]]
+typedef enum {
+  gsl_wavelet_forward = 1, gsl_wavelet_backward = -1
+}
+gsl_wavelet_direction;
 
+typedef struct
+{
+  const char *name;
+  int (*init) (const double **h1, const double **g1,
+               const double **h2, const double **g2, size_t * nc,
+               size_t * offset, size_t member);
+}
+gsl_wavelet_type;
+
+typedef struct
+{
+  const gsl_wavelet_type *type;
+  const double *h1;
+  const double *g1;
+  const double *h2;
+  const double *g2;
+  size_t nc;
+  size_t offset;
+}
+gsl_wavelet;
+
+typedef struct
+{
+  double *scratch;
+  size_t n;
+}
+gsl_wavelet_workspace;
+
+const gsl_wavelet_type *gsl_wavelet_daubechies;
+const gsl_wavelet_type *gsl_wavelet_daubechies_centered;
+const gsl_wavelet_type *gsl_wavelet_haar;
+const gsl_wavelet_type *gsl_wavelet_haar_centered;
+const gsl_wavelet_type *gsl_wavelet_bspline;
+const gsl_wavelet_type *gsl_wavelet_bspline_centered;
+
+gsl_wavelet *gsl_wavelet_alloc (const gsl_wavelet_type * T, size_t k);
+void gsl_wavelet_free (gsl_wavelet * w);
+const char *gsl_wavelet_name (const gsl_wavelet * w);
+
+gsl_wavelet_workspace *gsl_wavelet_workspace_alloc (size_t n);
+void gsl_wavelet_workspace_free (gsl_wavelet_workspace * work);
+
+int gsl_wavelet_transform (const gsl_wavelet * w,
+                           double *data, size_t stride, size_t n,
+                           gsl_wavelet_direction dir,
+                           gsl_wavelet_workspace * work);
+
+int gsl_wavelet_transform_forward (const gsl_wavelet * w,
+                                   double *data, size_t stride, size_t n,
+                                   gsl_wavelet_workspace * work);
+
+int gsl_wavelet_transform_inverse (const gsl_wavelet * w,
+                                    double *data, size_t stride, size_t n,
+                                    gsl_wavelet_workspace * work);
+
+]]
 
 if jit.os == 'Linux' then
 	return ffi.C
