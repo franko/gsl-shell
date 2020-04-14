2be7b9f6beceac342176bc5f98bdfdc4fee93aa1
Francesco Abbate <francesco.bbt@gmail.com>
2020-04-14 12:46:16 +0200

    Use C++ iostream in text-based front-end application
    The function getline is a GNU extension and is not available on Windows.
    
    Get advantage of C++ iostream and string to get more readable code and
    be cross-platform.

--- core/gsl-shell.cpp
+++ core/gsl-shell.cpp
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
+#include <iostream>
+#include <string>
 
 #include "core/welcome.h"
 #include "core/LuaInterpreter.h"
@@ -8,73 +7,35 @@
 const char *prompt = "> ";
 const char *prompt_continuation = ". ";
 
-char *add_to_multi_line(char *input, char *line) {
-    int line_len = strlen(line);
-    int existing_input_len = (input ? strlen(input) : 0);
-    const char *separator = " ";
-    int separator_len = 1;
-    char *new_input = (char *) malloc(existing_input_len + line_len + separator_len + 1);
-    char *new_input_pointer = new_input;
-    if (input) {
-        memcpy(new_input_pointer, input, existing_input_len);
-        new_input_pointer += existing_input_len;
-        memcpy(new_input_pointer, separator, separator_len);
-        new_input_pointer += separator_len;
-    }
-    memcpy(new_input_pointer, line, line_len + 1);
-    free(input);
-    return new_input;
-}
-
-char *dispose_multi_line(char *input) {
-    free(input);
-    return nullptr;
-}
-
 int main() {
     LuaInterpreter intepreter{LuaLanguage::kLanguageExtension};
     auto init_status = intepreter.Initialize();
     if (init_status != Interpreter::Result::kSuccess) {
-        fprintf(stderr, "Error initializing Lua\n");
+        std::cerr << "Error initializing Lua" << std::endl;
         return 1;
     }
-    fputs(kWelcomeMessage, stdout);
-    fputs("\n\n", stdout);
-    char *multi_line_input = nullptr;
-    char *line_buffer = nullptr;
-    size_t line_buffer_size = 0;
-    while (true) {
-        if (multi_line_input) {
-            fputs(prompt_continuation, stdout);
+    std::cout << kWelcomeMessage << std::endl << std::endl;
+    std::string multi_line;
+    bool multi_line_continued = false;
+    while (!std::cin.bad() && !std::cin.eof()) {
+        std::cout << (multi_line_continued ? prompt_continuation : prompt);
+        std::cout.flush();
+        std::string line;
+        std::getline(std::cin, line);
+        if (multi_line_continued) {
+            multi_line += " " + line;
         } else {
-            fputs(prompt, stdout);
+            multi_line = line;
         }
-        fflush(stdout);
-        ssize_t getline_return_code = getline(&line_buffer, &line_buffer_size, stdin);
-        if (getline_return_code < 0) {
-            int getline_errno = errno;
-            if (getline_errno != 0) {
-                const char *getline_error_string = strerror(getline_errno);
-                fprintf(stderr, "error reading line: %s\n", getline_error_string);
-            } else {
-                fputc('\n', stdout);
-            }
-            break;
-        } else {
-            ssize_t len = getline_return_code;
-            if (len > 0 && line_buffer[len - 1] == '\n') {
-                line_buffer[len - 1] = 0;
-            }
-            multi_line_input = add_to_multi_line(multi_line_input, line_buffer);
-            auto execute_result = intepreter.Execute(multi_line_input);
+        if (multi_line.length() > 0) {
+            auto execute_result = intepreter.Execute(multi_line.c_str());
             if (execute_result == Interpreter::Result::kError) {
-                const char *error_message = intepreter.ErrorMessageString();
-                fprintf(stderr, "Error reported on execution\n");
-                fprintf(stderr, "%s\n", error_message);
+                std::cout << intepreter.ErrorMessageString() << std::endl;
             }
-            if (execute_result != Interpreter::Result::kIncompleteInput) {
-                multi_line_input = dispose_multi_line(multi_line_input);
-            }
+            multi_line_continued = (execute_result == Interpreter::Result::kIncompleteInput);
+        }
+        if (std::cin.bad()) {
+            std::cerr << "error reading input" << std::endl;
         }
     }
     intepreter.Close();
