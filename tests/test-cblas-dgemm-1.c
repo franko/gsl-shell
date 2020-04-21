30fc944a8fec0d4690d07eb2d603c000dd803caa
Francesco Abbate <francesco.bbt@gmail.com>
2020-04-21 09:58:26 +0200

    Add check of matrix elements in cblas tests

--- tests/test-cblas-dgemm-1.c
+++ tests/test-cblas-dgemm-1.c
@@ -1,5 +1,6 @@
 #define min(x,y) (((x) < (y)) ? (x) : (y))
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "cblas.h"
@@ -11,6 +12,22 @@ static void print_matrix_top_left(double *A, int m, int n, int m_limit, int n_li
         }
         printf ("\n");
     }
+}
+
+static int check_matrix_values_top_left(double *A, int m, int n, int m_limit, int n_limit, const double *A_ref, const double eps_abs, const double eps_rel) {
+    int difference_seen = 0;
+    for (int i=0; i<min(m,m_limit); i++) {
+        for (int j=0; j<min(n,n_limit); j++) {
+            const double element_ref = A_ref[j+i*n_limit];
+            const double elements_diff = A[j+i*n] - element_ref;
+            if (fabs(elements_diff) > eps_abs || 
+                fabs(elements_diff / element_ref) > eps_rel) {
+                difference_seen += 1;
+                printf ("Difference for element: (%d; %d) value: %12.0f expected: %12.0f\n", i, j, A[j+i*n], element_ref);
+            }
+        }
+    }
+    return difference_seen;
 }
 
 int main() {
@@ -66,11 +83,22 @@ int main() {
     printf ("\n Top left corner of matrix C: \n");
     print_matrix_top_left(C, m, n, 6, 6);
 
+    const double eps_abs = 100, eps_rel = 1e-8;
+    const double C_expect[36] = {
+        -2666620100, -2666640200, -2666660300, -2666680400, -2666700500, -2666720600,
+        -6646660100, -6646720200, -6646780300, -6646840400, -6646900500, -6646960600,
+        -10626700100, -10626800200, -10626900300, -10627000400, -10627100500, -10627200600,
+        -14606740100, -14606880200, -14607020300, -14607160400, -14607300500, -14607440600,
+        -18586780100, -18586960200, -18587140300, -18587320400, -18587500500, -18587680600,
+        -22566820100, -22567040200, -22567260300, -22567480400, -22567700500, -22567920600,
+    };
+    int check = check_matrix_values_top_left(C, m, n, 6, 6, C_expect, eps_abs, eps_rel);
+
     printf ("\n Deallocating memory \n\n");
     free(A);
     free(B);
     free(C);
 
     printf (" Example completed. \n\n");
-    return 0;
+    return (check == 0 ? 0 : 1);
 }
