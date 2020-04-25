36a3acb830ff478e451f94552ed0ef37de3fe89e
Francesco Abbate <francesco.bbt@gmail.com>
2020-04-25 18:56:19 +0200

    Adding FFI bindings for lapacke

---
+++ lapack.lua
@@ -0,0 +1,6 @@
+local ffi = require("ffi")
+local openblas = require("cblas")
+local lapacke_file = io.open("lapacke_ffi.h", "rb")
+local lapacke_decls = lapacke_file:read("*all")
+ffi.cdef(lapacke_decls)
+return openblas
