e60d7edbd5fea93039916c0febc263ee4a570232
Francesco Abbate <francesco.bbt@gmail.com>
2020-04-27 11:39:12 +0200

    Add fix for openblas library name on Windows

--- cblas-ffi.lua
+++ cblas-ffi.lua
@@ -2,5 +2,5 @@ local ffi = require("ffi")
 local cblas_file = io.open("cblas_ffi.h", "rb")
 local cblas_decls = cblas_file:read("*all")
 ffi.cdef(cblas_decls)
-local openblas_lib = ffi.load("openblas")
+local openblas_lib = ffi.load((jit.os == "Windows" and "libopenblas" or "openblas"))
 return openblas_lib
