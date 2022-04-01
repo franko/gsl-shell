bc93580bb34e1d7ecb409a9fa8cd2e2d05d44be8
Francesco Abbate <francesco.bbt@gmail.com>
2022-04-01 18:11:53 +0200

    Improve run-local script and fix a problem
    Look for lua files i ndata folder since they were moved.

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -2,7 +2,36 @@
 
 set -o errexit
 
-builddir="$1"
+exe_name=gsl-shell-gui
+
+pargs=()
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -console)
+    exe_name=gsl-shell
+    ;;
+    -*)
+    echo "error: unknown option \"$1\""
+    exit 1
+    ;;
+    *)
+    pargs+=("$1")
+    ;;
+  esac
+  shift
+done
+
+if [ "${#pargs[@]}" -lt 1 ]; then
+  echo "usage: $0 [options] <build-dir>"
+  exit 1
+fi
+
+ext=""
+if [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "mingw"* ]]; then
+  ext=".exe"
+fi
+
+builddir="${pargs[0]}"
 
 rundir=".run"
 bindir="$rundir"
@@ -10,7 +39,8 @@ datadir="$rundir/lua"
 
 rm -fr "$rundir"
 mkdir -p "$bindir" "$datadir"
-cp "$builddir/fox-gui/gsl-shell-gui.exe" "$bindir"
+cp "$builddir/fox-gui/gsl-shell-gui$ext" "$bindir"
+cp "$builddir/gsl-shell$ext" "$bindir"
 
 lua_files=(bspline.lua fft-init.lua integ-init.lua template.lua check.lua
 graph-init.lua rng.lua rnd.lua randist.lua iter.lua time.lua gsl-check.lua linfit.lua
@@ -22,20 +52,20 @@ lm-helpers.lua algorithm.lua monomial.lua linfit_rank.lua matrix-power.lua base.
 )
 
 for name in "${lua_files[@]}"; do
-  cp "$name" "$datadir"
+  cp "data/$name" "$datadir"
 done
 
 mkdir -p "$datadir/templates"
 for name in gauss-kronrod-x-wgs qag rk8pd lmfit qng rkf45 ode-defs rk4 sf-defs vegas-defs rnd-defs; do
-    cp "templates/${name}.lua.in" "$datadir/templates"
+    cp "data/templates/${name}.lua.in" "$datadir/templates"
 done
 
 mkdir -p "$datadir/demos"
 for name in bspline fft plot wave-particle fractals ode nlinfit integ anim linfit contour svg graphics sf vegas gdt-lm; do
-    cp "demos/${name}.lua" "$datadir/demos"
+    cp "data/demos/${name}.lua" "$datadir/demos"
 done
 
 pushd "$rundir"
-./gsl-shell-gui
+./$exe_name
 popd
 
