69a431c7fc59700ff03323d93cb60fcf278ca0fd
Francesco Abbate <francesco.bbt@gmail.com>
2022-04-05 14:48:49 +0200

    Improve install config in script and meson

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -50,38 +50,7 @@ rm -fr "$rundir"
 mkdir -p "$bindir" "$datadir"
 cp "$builddir/src/console/gsl-shell$ext" "$bindir"
 cp "$builddir/src/fox-gui/gsl-shell-gui$ext" "$bindir"
-
-lua_files=(bspline.lua fft-init.lua integ-init.lua template.lua check.lua
-graph-init.lua rng.lua rnd.lua randist.lua iter.lua time.lua gsl-check.lua linfit.lua
-roots.lua contour.lua gsl.lua matrix.lua csv.lua start.lua num.lua demo-init.lua
-import.lua plot3d.lua sf.lua vegas.lua eigen.lua help.lua cgdt.lua expr-actions.lua
-expr-lexer.lua expr-parse.lua expr-print.lua gdt-factors.lua gdt-interp.lua gdt-expr.lua
-gdt-hist.lua gdt-lm.lua gdt.lua gdt-parse-csv.lua gdt-plot.lua lm-expr.lua
-lm-helpers.lua algorithm.lua monomial.lua linfit_rank.lua matrix-power.lua base.lua
-)
-
-for name in "${lua_files[@]}"; do
-  cp "data/$name" "$datadir"
-done
-
-mkdir -p "$datadir/templates"
-for name in gauss-kronrod-x-wgs qag rk8pd lmfit qng rkf45 ode-defs rk4 sf-defs vegas-defs rnd-defs; do
-    cp "data/templates/${name}.lua.in" "$datadir/templates"
-done
-
-mkdir -p "$datadir/demos"
-for name in bspline fft plot wave-particle fractals ode nlinfit integ anim linfit contour svg graphics sf vegas gdt-lm; do
-    cp "data/demos/${name}.lua" "$datadir/demos"
-done
-
-mkdir -p "$datadir/pre3d"
-cp data/pre3d/pre3d.lua data/pre3d/pre3d_shape_utils.lua "$datadir/pre3d"
-
-mkdir -p "$datadir/help"
-for name in fft graphics integ iter matrix nlfit ode rng vegas; do
-    cp "data/help/${name}.lua" "$datadir/help"
-done
-
+cp -r data/. "$datadir"
 cp resources/gsl-shell.desktop resources/gsl-shell.svg "$rundir"
 
 $strip "$bindir/gsl-shell$ext" "$bindir/gsl-shell-gui$ext"
