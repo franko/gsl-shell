5db16a1a19ec21713601967e91086f59bc51416d
Francesco Abbate <francesco.bbt@gmail.com>
2022-07-24 22:53:20 +0200

    Use project.run instead of activate function
    The new function project.run takes the script filename and chdir into
    its directory and add its path into project.path before running the
    script.
    
    The new function will cleanup when done to restore the application in
    its previous state for the working directory and the package.path.
    
    Replace the previous project.activate function.

--- data/project.lua
+++ data/project.lua
@@ -3,10 +3,12 @@ local ffi = require 'ffi'
 if ffi.os == "Windows" then
     ffi.cdef [[
         int _chdir(const char *path);
+        char *_getcwd(char *buffer, int maxlen);
     ]]
 else
     ffi.cdef [[
         int chdir(const char *path);
+        char *getcwd(char *buf, size_t size);
     ]]
 end
 
@@ -18,25 +20,41 @@ local function chdir(s)
         err = ffi.C.chdir(s)
     end
     if err ~= 0 then
-        error("chdir() into directory %q failed.")
+        error("chdir() into directory " .. s .. " failed.")
     end
 end
 
-function activate()
-    local info = debug.getinfo(2, "S")
-    if not info or not info.source then
-        print("not a script: no project directory to activate")
-        return
+local function getcwd()
+    local buf_size = 256
+    while buf_size <= 8192 do
+        local buf = ffi.new("char[?]", buf_size)
+        local r
+        if ffi.os == "Windows" then
+            r = ffi.C._getcwd(buf, buf_size)
+        else
+            r = ffi.C.getcwd(buf, buf_size)
+        end
+        if r then
+            return ffi.string(buf)
+        else
+            buf_size = buf_size * 4
+        end
     end
-    local str = info.source:sub(2)
-    local path = str:match("(.*[/\\])")
+    error("error calling getcwd()")
+end
+
+local function run(filename)
+    local cwd = getcwd()
+    local ppath = package.path
+    local path = filename:match("(.*[/\\])")
     if path then
         chdir(path)
         package.path = path .. "?.lua;" .. package.path
-    else
-        error("cannot locate project's directory")
     end
+    dofile(filename)
+    package.path = ppath
+    chdir(cwd)
 end
 
-return { chdir = chdir, activate = activate }
+return { chdir = chdir, getcwd = getcwd, run = run }
 
