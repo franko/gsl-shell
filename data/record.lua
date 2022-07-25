46d5f5813bb23592393e5afc6528f034d8e1495a
Francesco Abbate <francesco.bbt@gmail.com>
2022-07-25 19:23:08 +0200

    Implement intermediate values logger for ODE
    Introduce the record module to store time-dependent values and plot the
    stored values.
    
    The ODE solver now use the record module to let the ODE function store
    any kind of intermediate values using a call:
    
    log:store(t, {value1 = xx, value2 = xx})
    
    The "log" argument will be provided to the function as the last
    argument. If it is nil no values should be stored so the function should
    always check if it is nil before storing values.
    
    The log will be actually passed when the ODE solver crosses the t
    intermediate values required by the "evolve" function.

---
+++ data/record.lua
@@ -0,0 +1,51 @@
+local Record = {}
+
+Record.__index = Record
+
+function Record:store(t, values)
+    if not self.tab then
+        local hs = {"t"}
+        for k in pairs(values) do
+            hs[#hs + 1] = k
+        end
+        self.tab = gdt.new(0, hs)
+    end
+    values.t = t
+    self.tab:append(values)
+end
+
+local function record_plot(self, pll, as_canvas)
+    local np = #pll
+    local plots = {}
+    local w = graph.window("v" .. table.concat(iter.ilist(|| ".", np), ""))
+    for i = 1, np do
+        local p = (as_canvas and graph.canvas() or graph.plot())
+        for j, var_name in ipairs(pll[i]) do
+            local line = graph.path()
+            for k = 1, #self.tab do
+                line:line_to(self.tab:get(k, "t"), self.tab:get(k, var_name))
+            end
+            p:addline(line, graph.webcolor(j))
+            p:legend(var_name, graph.webcolor(j), "line")
+        end
+        w:attach(p, np - i + 1)
+        plots[i] = p
+    end
+    return plots
+end
+
+function Record:plot(...)
+    local arg1 = select(1, ...)
+    if type(arg1) == "string" then
+        local plots = record_plot(self, {{...}}, false)
+        return plots[1]
+    else
+        return record_plot(self, ...)
+    end 
+end
+
+function Record.new()
+    return setmetatable({ }, Record)
+end
+
+return Record
