e55af0463925deef9ad1da6cacdfceb8c9a5fb13
Francesco Abbate <francesco.bbt@gmail.com>
2022-10-25 16:30:49 +0200

    Add project activate function

--- data/project.lua
+++ data/project.lua
@@ -1,5 +1,7 @@
 local ffi = require 'ffi'
 
+local prev_package_path, prev_working_dir
+
 if ffi.os == "Windows" then
     ffi.cdef [[
         int _chdir(const char *path);
@@ -31,6 +33,15 @@ local function getcwd()
     return ffi.string(buf)
 end
 
+local function deactivate()
+    if prev_package_path then
+        package.path = prev_package_path
+        chdir(prev_working_dir)
+        prev_package_path = nil
+        prev_working_dir = nil
+    end
+end
+
 local function run(filename)
     local cwd = getcwd()
     local ppath = package.path
@@ -39,10 +50,30 @@ local function run(filename)
         chdir(path)
         package.path = path .. "?.lua;" .. package.path
     end
-    dofile(filename)
-    package.path = ppath
-    chdir(cwd)
+    prev_package_path = ppath
+    prev_working_dir = cwd
+    local ok, err = pcall(dofile, filename)
+    deactivate()
+    if not ok then error(err) end
 end
 
-return { chdir = chdir, getcwd = getcwd, run = run }
+local function activate(dirname)
+    deactivate()
+    local cwd = getcwd()
+    local prev_path = package.path
+    local path = dirname:match("(.*)[/\\]?$")
+    chdir(path)
+    local dirsep = package.config:sub(1, 1)
+    package.path = path .. dirsep .. "?.lua;" .. package.path
+    prev_package_path = prev_path
+    prev_working_dir = cwd
+end
+
+return {
+    chdir = chdir,
+    getcwd = getcwd,
+    run = run,
+    activate = activate,
+    deactivate = deactivate,
+}
 
